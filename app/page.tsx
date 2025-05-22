23c9ac2ea4656b15df07cc9d1797523e64ac9089
aroosa.alpha <aroosa.alphasquad@gmail.com>
2025-05-22 19:49:55 +0500

    feat(app): Fixed the clock

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,59 @@
 'use client';
-const numbers=[{
-  id:1, blocks: "⬛⬛⬛⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜\n",
-},
-{
-id:2, blocks: "⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬜⬜⬜⬜\n⬜⬛⬛⬛\n⬜⬜⬜⬜\n",
-},
-{
-  id:3, blocks: "⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬜⬜⬜⬜\n",
-},
-{
-  id:4, blocks:"⬜⬛⬛⬜\n⬜⬛⬛⬜\n⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜\n",
-},
-{
-  id:5, blocks:"⬜⬜⬜⬜\n⬜⬛⬛⬛\n⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬜⬜⬜⬜\n",
-},
-{
-  id:6, blocks:"⬜⬛⬛⬛\n⬜⬛⬛⬛\n⬜⬜⬜⬜\n⬜⬛⬛⬜\n⬜⬜⬜⬜\n",
-},
-{
-  id:7, blocks:"⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜\n",
-},
-{
-  id:8, blocks:"⬜⬜⬜⬜\n⬜⬛⬛⬜\n⬜⬜⬜⬜\n⬜⬛⬛⬜\n⬜⬜⬜⬜\n",
-},
-{
-  id:9, blocks:"⬜⬜⬜⬜\n⬜⬛⬛⬜\n⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬜⬜⬜⬜\n",
-},
-{
-  id:0, blocks:"⬜⬜⬜⬜\n⬜⬛⬛⬜\n⬜⬛⬛⬜\n⬜⬛⬛⬜\n⬜⬜⬜⬜\n",
-}
-
+const numbers=[
+{id:1, blocks: "⬛⬛⬛⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜",},
+{id:2, blocks: "⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬜⬜⬜⬜\n⬜⬛⬛⬛\n⬜⬜⬜⬜",},
+{id:3, blocks: "⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬜⬜⬜⬜",},
+{id:4, blocks: "⬜⬛⬛⬜\n⬜⬛⬛⬜\n⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜",},
+{id:5, blocks: "⬜⬜⬜⬜\n⬜⬛⬛⬛\n⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬜⬜⬜⬜",},
+{id:6, blocks: "⬜⬛⬛⬛\n⬜⬛⬛⬛\n⬜⬜⬜⬜\n⬜⬛⬛⬜\n⬜⬜⬜⬜",},
+{id:7, blocks: "⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜\n⬛⬛⬛⬜",},
+{id:8, blocks: "⬜⬜⬜⬜\n⬜⬛⬛⬜\n⬜⬜⬜⬜\n⬜⬛⬛⬜\n⬜⬜⬜⬜",},
+{id:9, blocks: "⬜⬜⬜⬜\n⬜⬛⬛⬜\n⬜⬜⬜⬜\n⬛⬛⬛⬜\n⬜⬜⬜⬜",},
+{id:0, blocks: "⬜⬜⬜⬜\n⬜⬛⬛⬜\n⬜⬛⬛⬜\n⬜⬛⬛⬜\n⬜⬜⬜⬜",}
 ]
-const unit =[]
-const unitTime= []
-
 function formatTimeUnit(unit) {
   return unit.toString().padStart(2, "0");
-
 }
-
 function displayClock() {
+  const unit =[]
+  const unitTime= []
+  const unitDate=[]
   const now = new Date();
   const h = formatTimeUnit(now.getHours());
   const m = formatTimeUnit(now.getMinutes());
   const s = formatTimeUnit(now.getSeconds());
-
-  //const time = `${h} : ${m} : ${s}`;
-  const time = `19 : 30 : 00`;
-
-console.clear();
-console.log(h,":",m,":",s)
-var rollingConcatenation = '';
-console.log("time", time)
-for(const char of time){
-  if(char===":"){
-      unit.push("⬛\n⬜\n⬛\n⬜\n⬛\n")
-    continue;
-    }
-    
-    else if(char===" "){
-      unit.push("⬛\n⬛\n⬛\n⬛\n⬛\n")
+  var time = `${h.split("").join(" ")} : ${m.split("").join(" ")} : ${s.split("").join(" ")}`;
+  console.clear();
+  var rollingConcatenation = '';
+  for(const char of time){
+    if(char===":"){
+        unit.push("⬛\n⬜\n⬛\n⬜\n⬛")
       continue;
-    }
-
-  for(const number of numbers){
-    if(number.id===Number(char)){
-      //console.log("Character", char
-      unit.push(number.blocks);
-      
-      //console.log(number.blocks);
-    }
-    
-  } 
-}
-//console.log("unit", unit)
-unit.forEach((item, index) => {
-  for(const char of item){
-    //console.log("Character in unit", char)
-    if(char !=="\n"){
-          unitTime.push[char]
-      
-    }
-    else ;
-    rollingConcatenation += char;
-    //console.log("RollingConcatenation", rollingConcatenation)
-
+      } 
+      else if(char===" "){
+        unit.push("⬛\n⬛\n⬛\n⬛\n⬛")
+        continue;
+      }
+    for(const number of numbers){
+      if(number.id===Number(char)){
+        unit.push(number.blocks);
+      } 
+    } 
   }
-   
-  
-});
-//console.log("unitTime" ,unitTime)
-console.log(rollingConcatenation)
-//console.groupEnd();
-
-
- // console.log("This is time string",time)
-
+  unit.forEach((item, index) => {
+    var joined = "";
+    const pieces = item.split("\n");
+    unitTime.push(pieces);
+  })
+    for(let i=0; i<5; i++)
+    {
+      for(let j=0; j<unitTime.length; j++)
+      {
+        rollingConcatenation += unitTime[j][i];
+      }
+      unitDate.push(rollingConcatenation);
+      console.log(rollingConcatenation)
+      rollingConcatenation = '';
+    }
 }
-
-displayClock();
+setInterval(displayClock, 1000)
